d31c9075eedbe11e5009766690a0e90e397a4560
Mike Bulanov <mikki.messer@gmail.com>
2022-04-21 00:38:29 +0300

    Text fields and radio/checkboxes filled

--- src/test/java/com/mikkimesser/AutomationPracticeFormTests.java
+++ src/test/java/com/mikkimesser/AutomationPracticeFormTests.java
@@ -1,8 +1,10 @@
 package com.mikkimesser;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.commands.ScrollTo;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -12,12 +14,17 @@ public class AutomationPracticeFormTests {
     @BeforeAll static void setUp() {
         Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.browserSize = "1280x720)";
-
     }
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
         $("#firstName").setValue("TestName");
+        $("#lastName").setValue("TestLastName");
+        $("#userEmail").setValue("mail@example.com");
+        $("[for=gender-radio-1]").click();
+        $("#userNumber").setValue("1234567890");
+        $("[for=hobbies-checkbox-2").click();
+        $("#currentAddress").scrollIntoView(false);
+        $("#currentAddress").setValue("47 QA Street");
     }
 }
