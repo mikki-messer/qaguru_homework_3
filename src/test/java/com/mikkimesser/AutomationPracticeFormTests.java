db484d10773eb5effc890c350912e67cec56a298
Mike Bulanov <mikki.messer@gmail.com>
2022-04-20 23:01:45 +0300

    FirstName value set

--- src/test/java/com/mikkimesser/AutomationPracticeFormTests.java
+++ src/test/java/com/mikkimesser/AutomationPracticeFormTests.java
@@ -1,14 +1,23 @@
 package com.mikkimesser;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class AutomationPracticeFormTests {
 
+    @BeforeAll static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        //Configuration.browserSize = "1280x720)";
+
+    }
     @Test
-    void fillFormTest()
-    {
-        open("https://demoqa.com/automation-practice-form");
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("TestName");
     }
 }
