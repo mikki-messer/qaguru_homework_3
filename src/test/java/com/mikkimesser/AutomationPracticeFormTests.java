71d511e8532c2f2a988df30ed24ef9ebbbab3963
Mike Bulanov <mikki.messer@gmail.com>
2022-05-01 18:30:57 +0300

    added faker and faked the variables

--- src/test/java/com/mikkimesser/AutomationPracticeFormTests.java
+++ src/test/java/com/mikkimesser/AutomationPracticeFormTests.java
@@ -2,14 +2,24 @@ package com.mikkimesser;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.DateAndTime;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTests {
+
+    Faker faker = new Faker();
 
     @BeforeAll static void setUp() {
         Configuration.holdBrowserOpen = true;
@@ -18,15 +28,23 @@ public class AutomationPracticeFormTests {
     }
 
     @Test
-    void fillFormTest() {
-        String firstName = "Harry";
-        String lastName = "Dirty";
-        String email = "mail@example.com";
-        String phoneNumber = "1234567890";
-        String year = "1983";
-        String month = "August";
-        String dt = "31";
-        String address = "47 QA Street";
+    void fillFormTest() throws ParseException {
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String email = faker.internet().emailAddress();
+        String phoneNumber = faker.phoneNumber().subscriberNumber(10);
+
+        //faking dates
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date randomDate = faker.date().between(simpleDateFormat.parse("1900-01-01"),
+                simpleDateFormat.parse("2010-01-01"));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(randomDate);
+        String year = String.valueOf(calendar.get(Calendar.YEAR));
+        String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        String dt = String.format("%02d", calendar.get(Calendar.DAY_OF_MONTH));
+
+        String address = faker.address().fullAddress();
         String state = "NCR";
         String city = "Gurgaon";
         String pictureName = "test.jpeg";
@@ -45,6 +63,7 @@ public class AutomationPracticeFormTests {
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
         $(byText(gender)).click();
+
         $("#userNumber").setValue(phoneNumber);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption(month);
